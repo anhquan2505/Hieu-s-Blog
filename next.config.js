b401491664fc96271611fce204073eb3debac4ed
anhquan2505 <quanquytoc25052000@gmail.com>
2023-10-21 11:35:31 +0700

    deploy v3

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     unoptimized: true 
   },
   output: 'export',
- 
+  
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
   // trailingSlash: true,
  
