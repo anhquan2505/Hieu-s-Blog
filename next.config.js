61835be3a02832287c8499b80f3e90c992096de4
anhquan2505 <quanquytoc25052000@gmail.com>
2023-10-21 10:43:25 +0700

    deploy

--- next.config.js
+++ next.config.js
@@ -1,8 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images:{
-    domains:["lh3.googleusercontent.com","firebasestorage.googleapis.com"]
-  }
+    
+  },
+  output: 'export',
+ 
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+ 
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+ 
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
 }
 
 module.exports = nextConfig
