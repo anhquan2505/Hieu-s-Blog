f35aef4a228ca6369ad5ec471037ba4565380f13
anhquan2505 <quanquytoc25052000@gmail.com>
2023-10-21 11:16:33 +0700

    hello

--- src/app/posts/[slug]/page.jsx
+++ src/app/posts/[slug]/page.jsx
@@ -1,59 +1,15 @@
-"use client"
-import Menu from "@/components/Menu/Menu";
-import styles from "./singlePage.module.css";
-import Image from "next/image";
-// import { useEffect } from "react";
-import { useParams } from 'next/navigation'
-import { useContext } from "react";
-import { DatabaseContext } from "@/context/DatabaseContext";
 
+
+import SinglePageContainer from "@/components/singlePageContainer/singlePageContainer";
 // import Comments from "@/components/comments/Comments";
 
 
 
 const SinglePage =  () => {
-  let { slug } = useParams()
-  slug = parseInt(slug)
   
-  // const  = router.params
-  console.log(slug);
-  const database = useContext(DatabaseContext)
-  console.log(database);
-  if (database.length == 0 && slug>= database.length && slug<0){
-    return (<div>Loading</div>)
-  }
-  
-  const {title,desc} = database[slug]
   return (
-    <div className={styles.container}>
-      <div className={styles.infoContainer}>
-        <div className={styles.textContainer}>
-          <h1 className={styles.title}>{title}</h1>
-          <div className={styles.user}>
-              <div className={styles.userImageContainer}>
-                <Image src="/logo.png" alt="Hello" fill className={styles.avatar} />
-              </div>
-            
-            <div className={styles.userTextContainer}>
-              <span className={styles.username}>Hiếu</span>
-              <span className={styles.date}>15.10.2023</span>
-            </div>
-          </div>
-        </div>
-        
-          <div className={styles.imageContainer}>
-            <Image src="/food.png" alt="" fill className={styles.image} />
-          </div>
-        
-      </div>
-      <div className={styles.content}>
-        <div className={styles.post} >
-          {desc.map((section,index)=>(
-            <div className={styles.description} key={"section"+index} >{section}</div>
-          ))}
-        </div>
-        <Menu />
-      </div>
+    <div>
+      <SinglePageContainer></SinglePageContainer>
     </div>
   );
 };
